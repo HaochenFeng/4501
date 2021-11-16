7a11601d0a4651da05550a799f324341c158d1b3
HaochenFeng <93166300+HaochenFeng@users.noreply.github.com>
2021-11-15 19:59:37 -0500

    Create Portfolio.py

---
+++ Portfolio.py
@@ -0,0 +1,26 @@
+class Portfolio:
+    def __inif__(self):
+        self.stocks = {} 
+
+    def buy(self, name, shares, price):
+        stock = {"name": name, "shares": shares, "price": price}
+        self.stocks[name] = stock
+
+    def cost(self, name):
+        stock = self.stocks[name]
+        shares = stock["shares"]
+        price = stock["price"]
+        cost = shares * price
+        return cost
+
+"""
+Within it, define a new class called Portfolio which:
+has a method called buy, which adds a new stock to the portfolio, taking 3 arguments
+name, a str, the symbol of the stock which is being bought
+shares, an int, the quantity which is being bought
+price, a float, the price paid per share
+and has a second method called cost which returns a float, the total cost paid for all stocks in the portfolio
+Consider that to implement the cost method, you'll need to be storing the purchases made each time the buy method is called somewhere.
+ You may do so by any means convenient to you.
+Commit this file to your repository and push it to GitHub using GitHub Desktop, with a suitable commit message.
+"""
