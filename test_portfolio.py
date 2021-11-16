b7d1eea3865e47b15f19f6f9a94badb6596f79e1
HaochenFeng <93166300+HaochenFeng@users.noreply.github.com>
2021-11-15 21:16:52 -0500

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -15,3 +15,14 @@ def test_buy_two_stock():
     p.buy("IBM", 100, 176.48)
     p.buy("HPQ", 100, 36.15)
     assert p.cost() == 21263.0
+
+def test_not_enough_argument_to_buy():
+    p = Portfolio()
+    
+    try:
+        p.buy("IBM")
+    except TypeError:
+        pass
+    else:
+        assert False
+    
